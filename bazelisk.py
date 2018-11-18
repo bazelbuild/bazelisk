f00b08a377024ad52508e386b594a92900d9e125
Philipp Wollermann <philwo@google.com>
2018-11-18 12:56:04 +0100

    Fix style nits

--- bazelisk.py
+++ bazelisk.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''
+"""
 Copyright 2018 Google Inc. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,16 +13,16 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-'''
+"""
 
-from distutils.version import LooseVersion
 from contextlib import closing
+from distutils.version import LooseVersion
 import json
-import platform
 import os.path
+import platform
 import re
-import subprocess
 import shutil
+import subprocess
 import sys
 import time
 
@@ -127,9 +127,9 @@ def download_bazel_into_directory(version, directory):
 
 
 def maybe_makedirs(path):
-  '''
+  """
   Creates a directory and its parents if necessary.
-  '''
+  """
   try:
     os.makedirs(path)
   except OSError as e:
