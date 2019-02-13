5f93c54d0c29ba14187394c625b0fb688a8c88fd
Yun Peng <pcloudy@google.com>
2019-02-13 14:48:09 +0100

    Optimize the output of `bazelisk --migrate ...` for Buildkite
    1. sort all incompatible flags
    2. Use `---` to print indiviual Bazel run output as a collapsed group
    3. Use `+++` to print the result as a expanded group

--- bazelisk.go
+++ bazelisk.go
@@ -365,6 +365,8 @@ func getIncompatibleFlags(bazeliskHome, resolvedBazelVersion string) ([]string, 
 		}
 	}
 
+	sort.Strings(result)
+
 	return result, nil
 }
 
@@ -372,7 +374,7 @@ func getIncompatibleFlags(bazeliskHome, resolvedBazelVersion string) ([]string, 
 func migrate(bazelPath string, baseArgs []string, newArgs []string) {
 	// 1. Try with all the flags.
 	args := append(baseArgs, newArgs...)
-	fmt.Printf("Running Bazel with %q\n", args)
+	fmt.Printf("\n\n--- Running Bazel with %q\n\n", args)
 	exitCode, err := runBazel(bazelPath, args)
 	if err != nil {
 		log.Fatalf("could not run Bazel: %v", err)
@@ -384,8 +386,7 @@ func migrate(bazelPath string, baseArgs []string, newArgs []string) {
 
 	// 2. Try with no flags, as a sanity check.
 	args = baseArgs
-	fmt.Printf("\n---\n\n")
-	fmt.Printf("Running Bazel with %q\n", args)
+	fmt.Printf("\n\n--- Running Bazel with %q\n\n", args)
 	exitCode, err = runBazel(bazelPath, args)
 	if err != nil {
 		log.Fatalf("could not run Bazel: %v", err)
@@ -400,8 +401,7 @@ func migrate(bazelPath string, baseArgs []string, newArgs []string) {
 	var failList []string
 	for _, arg := range newArgs {
 		args = append(baseArgs, arg)
-		fmt.Printf("\n---\n\n")
-		fmt.Printf("Running Bazel with %q\n", args)
+		fmt.Printf("\n\n--- Running Bazel with %q\n\n", args)
 		exitCode, err = runBazel(bazelPath, args)
 		if err != nil {
 			log.Fatalf("could not run Bazel: %v", err)
@@ -414,7 +414,7 @@ func migrate(bazelPath string, baseArgs []string, newArgs []string) {
 	}
 
 	// 4. Print report
-	fmt.Printf("\n---\n\n")
+	fmt.Printf("\n\n+++ Result\n\n")
 	fmt.Printf("Command was successful with the following flags:\n")
 	for _, arg := range passList {
 		fmt.Printf("  %s\n", arg)
