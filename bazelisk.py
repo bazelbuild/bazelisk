1307aabc6df383646a0e88b95b8875c8ec5615c8
Marcel Hlopko <hlopko@google.com>
2019-05-20 09:24:50 +0200

    Support multidigit RC numbers

--- bazelisk.py
+++ bazelisk.py
@@ -220,7 +220,7 @@ def determine_url(version, is_commit, bazel_filename):
 
     # Split version into base version and optional additional identifier.
     # Example: '0.19.1' -> ('0.19.1', None), '0.20.0rc1' -> ('0.20.0', 'rc1')
-    (version, rc) = re.match(r"(\d*\.\d*(?:\.\d*)?)(rc\d)?", version).groups()
+    (version, rc) = re.match(r"(\d*\.\d*(?:\.\d*)?)(rc\d+)?", version).groups()
     return "https://releases.bazel.build/{}/{}/{}".format(
         version, rc if rc else "release", bazel_filename
     )
