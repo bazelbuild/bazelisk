40d8ae3b575950523ff42752e0c465a73af8d538
Cody Vandermyn <721474+codeman9@users.noreply.github.com>
2022-06-15 05:13:18 -0700

    Download native macOS arm64 binary on M1 (#303)
    * Download native macOS arm64 binary on M1
    
    * Move logic to platforms.go
    
    * Support declared platforms with arm64
    
    * Minor fixes

--- test.sh
+++ test.sh
@@ -3,13 +3,15 @@
 set -euxo pipefail
 
 rm -rf "$HOME/Library/Caches/bazelisk"
-env -u USE_BAZEL_VERSION ./bin/bazelisk-darwin-amd64 version
-USE_BAZEL_VERSION="latest" ./bin/bazelisk-darwin-amd64 version
+
+arch=$(uname -m)
+env -u USE_BAZEL_VERSION ./bin/bazelisk-darwin-"$arch" version
+USE_BAZEL_VERSION="latest" ./bin/bazelisk-darwin-"$arch" version
 USE_BAZEL_VERSION="0.28.0" ./bin/bazelisk-darwin-amd64 version
-USE_BAZEL_VERSION="last_green" ./bin/bazelisk-darwin-amd64 version
-USE_BAZEL_VERSION="last_downstream_green" ./bin/bazelisk-darwin-amd64 version
-USE_BAZEL_VERSION="last_rc" ./bin/bazelisk-darwin-amd64 version
-USE_BAZEL_VERSION="bazelbuild/latest" ./bin/bazelisk-darwin-amd64 version
+USE_BAZEL_VERSION="last_green" ./bin/bazelisk-darwin-"$arch" version
+USE_BAZEL_VERSION="last_downstream_green" ./bin/bazelisk-darwin-"$arch" version
+USE_BAZEL_VERSION="last_rc" ./bin/bazelisk-darwin-"$arch" version
+USE_BAZEL_VERSION="bazelbuild/latest" ./bin/bazelisk-darwin-"$arch" version
 USE_BAZEL_VERSION="bazelbuild/0.27.0" ./bin/bazelisk-darwin-amd64 version
-USE_BAZEL_VERSION="philwo/latest" ./bin/bazelisk-darwin-amd64 version
+USE_BAZEL_VERSION="philwo/latest" ./bin/bazelisk-darwin-"$arch" version
 USE_BAZEL_VERSION="philwo/0.25.0" ./bin/bazelisk-darwin-amd64 version
