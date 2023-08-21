bazelbuild/bazelisk

commit 9f6bb90ebd5d892071911f42bcdaaaf4e29175b9

update rules_nodejs version (#348)
* update rules_nodejs version

* apparently we need some more loads

* update package name

BUILD
@@ -193,6 +193,7 @@ genrule(
 
 pkg_npm(
     name = "npm_package",
+    package_name = "@bazel/bazelisk",
     srcs = [
         "LICENSE",
         "README.md",

WORKSPACE
@@ -57,9 +57,13 @@ gazelle_dependencies()
 # We don't use any nodejs but this includes a rule for publishing releases to npm
 http_archive(
     name = "build_bazel_rules_nodejs",
-    sha256 = "84abf7ac4234a70924628baa9a73a5a5cbad944c4358cf9abdb4aab29c9a5b77",
-    urls = ["https://github.com/bazelbuild/rules_nodejs/releases/download/1.7.0/rules_nodejs-1.7.0.tar.gz"],
+    sha256 = "f10a3a12894fc3c9bf578ee5a5691769f6805c4be84359681a785a0c12e8d2b6",
+    urls = ["https://github.com/bazelbuild/rules_nodejs/releases/download/5.5.3/rules_nodejs-5.5.3.tar.gz"],
 )
+
+load("@build_bazel_rules_nodejs//:repositories.bzl", "build_bazel_rules_nodejs_dependencies")
+
+build_bazel_rules_nodejs_dependencies()
 
 load("@build_bazel_rules_nodejs//:index.bzl", "node_repositories")
 

commit e1e5474eb7a6afb1498795b9edd7ca2132d684b7

fix(npm): replace version placeholder with stamp value
My guess is this was broken by https://github.com/bazelbuild/bazelisk/pull/348

BUILD
@@ -154,6 +154,7 @@ go_binary(
 pkg_npm(
     name = "npm_package",
     package_name = "@bazel/bazelisk",
+    substitutions = {"0.0.0-PLACEHOLDER": "{BUILD_SCM_VERSION}"},
     srcs = [
         "LICENSE",
         "README.md",