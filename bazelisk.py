e24ba47e0ad1c0f47de16281b077530f955ca906
celestialorb <chris.m.busby@gmail.com>
2023-09-08 15:24:56 -0700

    stupid simple retry logic for download

--- bazelisk.py
+++ bazelisk.py
@@ -180,8 +180,7 @@ def get_version_history(bazelisk_directory):
         ),
         # This only handles versions with numeric components, but that is fine
         # since prerelease versions have been excluded.
-        key=lambda version: tuple(int(component)
-                                  for component in version.split('.')),
+        key=lambda version: tuple(int(component) for component in version.split(".")),
         reverse=True,
     )
 
@@ -339,21 +338,30 @@ def download_bazel_into_directory(version, is_commit, directory):
     return destination_path
 
 
-def download(url, destination_path):
-    sys.stderr.write("Downloading {}...\n".format(url))
-    request = Request(url)
-    if "BAZELISK_BASE_URL" in os.environ:
-        parts = urlparse(url)
-        creds = None
+def download(url, destination_path, retries=5, wait_seconds=5):
+    while retries > 0:
+        retries -= 1
         try:
-            creds = netrc.netrc().hosts.get(parts.netloc)
+            sys.stderr.write("Downloading {}...\n".format(url))
+            request = Request(url)
+            if "BAZELISK_BASE_URL" in os.environ:
+                parts = urlparse(url)
+                creds = None
+                try:
+                    creds = netrc.netrc().hosts.get(parts.netloc)
+                except Exception:
+                    pass
+                if creds is not None:
+                    auth = base64.b64encode(("%s:%s" % (creds[0], creds[2])).encode("ascii"))
+                    request.add_header("Authorization", "Basic %s" % auth.decode("utf-8"))
+
+            with closing(urlopen(request)) as response, open(destination_path, "wb") as file:
+                shutil.copyfileobj(response, file)
+                return
         except Exception:
-            pass
-        if creds is not None:
-            auth = base64.b64encode(("%s:%s" % (creds[0], creds[2])).encode("ascii"))
-            request.add_header("Authorization", "Basic %s" % auth.decode("utf-8"))
-    with closing(urlopen(request)) as response, open(destination_path, "wb") as file:
-        shutil.copyfileobj(response, file)
+            if retries <= 0:
+                raise
+            time.sleep(wait_seconds)
 
 
 def get_bazelisk_directory():
